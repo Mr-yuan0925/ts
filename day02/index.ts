8127dc7cfa6315bb5c6265be6d692354ad9fee0a
袁广纪 <6512305+yuan_guang_ji@user.noreply.gitee.com>
2023-04-07 17:32:31 +0800

    day01

---
+++ day02/index.ts
@@ -0,0 +1,16 @@
+let carName: string = 'Audi'
+
+interface Point {
+    x: number,
+    y: number
+}
+
+const point:Point= {
+    x: 18,
+    y: 5,
+}
+
+// const point2:Point= {
+//     x: 18,
+//     y: '5',
+// }
