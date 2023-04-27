62707f1fde98c09256a9e55160d03bd619c2cba7
袁广纪 <6512305+yuan_guang_ji@user.noreply.gitee.com>
2023-04-27 14:57:04 +0800

    修改目录结构

---
+++ Chapter02/day01/src/crowller.ts
@@ -0,0 +1,43 @@
+// ts -> .d.ts 翻译文件 -> js\
+import fs from "fs";
+import path from "path";
+import superagent from 'superagent'
+import Other from './other'
+
+export interface OtherType {
+    analyze: (html:string,filePath: string) => string
+}
+
+
+class Crowller {
+    private filePath = path.resolve(__dirname, '../data/course.json');
+    // 获取页面的html
+    private rawHtml = '';
+
+    async getRwaHTML() {
+        const result = await superagent.get(this.url);
+        return result.text
+    }
+
+
+
+    writeFile(content: string){
+        fs.writeFileSync(this.filePath,JSON.stringify(content))
+    }
+
+    async initSpiderProcess() {
+        const html = await this.getRwaHTML();
+        const fileContent = other.analyze(html, this.filePath)
+        this.writeFile(JSON.stringify(fileContent));
+
+    }
+    constructor(private url: string, private other: OtherType) {
+        this.initSpiderProcess();
+    }
+}
+
+const secret = "secretKey";
+const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
+
+const other = new Other();
+new Crowller(url,other);
